537ca9eb4e42bcba7aac4226cf074cadc0a7d5d4
konstantinkoetzner <171531275+konstantinkoetzner@users.noreply.github.com>
2024-06-21 00:08:08 +0000

    Add photo info page

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"net/http"
+	"os"
 )
 
 var tpl *template.Template
@@ -14,6 +16,7 @@ func init() {
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/info", infoHandler)
 	mux.HandleFunc("/photo/", photoHandler)
 
 	http.ListenAndServe(":8080", mux)
@@ -25,4 +28,28 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 func photoHandler(w http.ResponseWriter, r *http.Request) {
 	http.StripPrefix("/photo", http.FileServer(http.Dir("./photo/")))
+}
+
+type PhotoInfo struct {
+	Number   int    `json:"number"`
+	Location string `json:"location"`
+	Season   string `json:"season"`
+	Year     int    `json:"year"`
+	Camera   string `json:"camera"`
+	FileName string `json:"fileName"`
+}
+
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	f, err := os.ReadFile("photo/info.json")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	info := []PhotoInfo{}
+	err = json.Unmarshal(f, &info)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tpl.ExecuteTemplate(w, "info.gohtml", info)
 }
